3ab3d305d6fe80aff4983ccc516effc44cd9e8b0
Xiangyu wang <xiangyuwang@XiangyudeMacBook-Pro.local>
2024-10-13 22:14:56 +0800

    提交
    提交

--- perlite/content.php
+++ perlite/content.php
@@ -13,6 +13,13 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/permissions.php';
 include('helper.php');
 
+// 检查用户是否已登录
+if (!isset($_SESSION['user_id'])) {
+    // 用户未登录，重定向到登录页面
+    header("Location: login.php");
+    exit();
+}
+
 // check get params
 if (isset($_GET['mdfile'])) {
 	$requestFile = $_GET['mdfile'];
